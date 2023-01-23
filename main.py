e5c5463f7ba46e007b77d0ec1f3b3b3065b116ef
Muhammad Bilal <Mohammadbilalmirza07@gmail.com>
2023-01-23 12:29:38 +0500

    aws sdk boto3 added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-"""
-Module Docstring
-"""
+# pylint: disable=missing-module-docstring
 
-__author__ = "RJTM AWS LAMBDA"
+__author__ = "Muhammad Bilal"
 __version__ = "0.1.0"
+
+import boto3
+
+s3 =  boto3.resource('s3')
 
 def main():
     """ Main entry point of the app """
