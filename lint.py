5afd996c0e7eae1ec6a56ecec68edcbb750d2d4d
Muhammad Bilal <Mohammadbilalmirza07@gmail.com>
2023-01-23 10:52:29 +0500

    first commit

---
+++ lint.py
@@ -0,0 +1,16 @@
+import sys
+
+from pylint import lint
+
+THRESHOLD = 8
+
+run = lint.Run(["main.py"], do_exit=False)
+
+score = run.linter.stats["global_note"]
+
+if score < THRESHOLD:
+    print("Linter failed: Score < 8")
+
+    sys.exit(1)
+
+sys.exit(0)
---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "RJTM AWS LAMBDA"
+__version__ = "0.1.0"
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+main()
